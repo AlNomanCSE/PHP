a1af8cd66a7146ebffbe5b7ae64e346f23f946b5
Abdullah Al Noman <Abdullah.Noman@dizishore.com>
2025-06-11 23:40:34 +0600

    first commit

--- 1 introduction/index.php
+++ 1 introduction/index.php
@@ -14,24 +14,6 @@ foreach ($fruits as $index => $fruit) {
     echo "Index $index: $fruit </br>";
 }
 
+print_r(count($fruits))
+
 ?>
-<!-- <!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?= $title ?></title>
-    <script src="https://cdn.tailwindcss.com"></script>
-</head>
-
-<body>
-    <h1 class="text-3xl font-bold text-blue-600">
-        <?= $headLine . $title ?>
-    </h1>
-    <p class="text-gray-700 mt-4">
-        <?= 'Learn PHP' ?>
-    </p>
-</body>
-
-</html> -->
