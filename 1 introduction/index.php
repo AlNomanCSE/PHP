694e435b8085260ef710ea15f2d0aa5e05c7aeb7
Abdullah Al Noman <Abdullah.Noman@dizishore.com>
2025-06-12 12:27:22 +0600

    readme file added

--- 1 introduction/index.php
+++ 1 introduction/index.php
@@ -5,15 +5,15 @@ $title = "My Website";
 // =============================================================================
 $fruits = ["apple", "banana", "orange"];
 // var_dump($fruits);
-print_r($fruits);
-echo  "</br>";
-foreach($fruits as $fruit){
-   echo $fruit . "</br>";
-}
-foreach ($fruits as $index => $fruit) {
-    echo "Index $index: $fruit </br>";
-}
+// print_r($fruits);
+// echo  "</br>";
+// foreach($fruits as $fruit){
+//    echo $fruit . "</br>";
+// }
+// foreach ($fruits as $index => $fruit) {
+//     echo "Index $index: $fruit </br>";
+// }
 
-print_r(count($fruits))
+echo count($fruits);
 
 ?>
